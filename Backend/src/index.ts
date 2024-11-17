39cd6bc6662d2681a0b0e216b3afd99fe41e2cd1
TzurelRauper <tzurel150@gmail.com>
2024-11-17 15:35:52 +0200

    add api directory

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import connectDB from './config/dbConfig';
+import dotenv from 'dotenv';
 
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 connectDB();
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
