f4b959269c6081d24db9cec3dc2afc0f4278fbfd
TzurelRauper <tzurel150@gmail.com>
2024-11-18 10:14:51 +0200

    add schema and interface for question

--- Backend/src/api/models/schemas/userSchema.ts
+++ Backend/src/api/models/schemas/userSchema.ts
@@ -1,5 +1,5 @@
 import {Schema, model} from "mongoose";
-import { IUser } from "../../../interfaces";
+import { IUser, IQuestion } from "../../../interfaces";
 
 const userSchema: Schema = new Schema<IUser> ({
     nickname: {type: String, required: true, unique: true},
@@ -10,5 +10,15 @@ const userSchema: Schema = new Schema<IUser> ({
     password: {type: String, required: true},
 });
 
+const questionSchema: Schema = new Schema<IQuestion> ({
+    title: {type: String, required: true},
+    content: {type: String, required: true},
+    tags: {type: [String], required: true},
+    authorNickname: {type: String, required: true},
+    createdAt: {type: Date, required: true, default: Date.now},
+
+})
+
 const User = model<IUser>('User', userSchema);
-export default User;
+const Question = model<IQuestion>('Question', questionSchema);
+export {User, Question};
--- Backend/src/interfaces.ts
+++ Backend/src/interfaces.ts
@@ -6,3 +6,11 @@ export interface IUser extends Document {
     email: string;
     password: string;
 };
+
+export interface IQuestion extends Document {
+    title: string;
+    content: string;
+    tags: string[];
+    authorNickname: string;
+    createdAt: Date;
+}
